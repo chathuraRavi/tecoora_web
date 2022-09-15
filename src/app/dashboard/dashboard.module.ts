38425b1f03b94378e5ee0afc7bb7f433f2e2fb58
CHATHURA <chathura@champitsolutions.com>
2022-09-15 21:40:15 +0530

    add template

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HighchartsChartModule } from 'highcharts-angular';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+
+import { ECommerceComponent } from './e-commerce/e-commerce.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
+
+
+@NgModule({
+  declarations: [
+    AnalyticsComponent, 
+    ECommerceComponent
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    PerfectScrollbarModule,
+    HighchartsChartModule
+  ]
+})
+export class DashboardModule { }
